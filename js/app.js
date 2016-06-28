f2b5eab66ccd8565b60c093d67b067133d0f42db
andrew gibbs <andrewgibbs@outlook.com>
2016-06-27 19:09:53 -0700

    added observable functionality

--- js/app.js
+++ js/app.js
@@ -3,9 +3,21 @@ var ViewModel = function (){
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/phtos/big');
+  this.level = ko.observable('newBorn');
+  this.nickNames = ko.observableArray(['Mittens','Bubbles','Cuteness']);
 
   this.incrementCounter = function(){
-    this.clickCount(this.clickCount()+1);
+    if(this.clickCount()>5 && this.clickCount()<10){
+      this.clickCount(this.clickCount()+1);
+      this.level('infant');
+    }
+    else if(this.clickCount()>10){
+      this.clickCount(this.clickCount()+1);
+      this.level('teen');
+    }
+    else {
+      this.clickCount(this.clickCount()+1);
+    }
   };
 }
 
