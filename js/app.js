554bac4cb87fcdecd3f898e7194e5fa044b20440
andrew gibbs <andrewgibbs@outlook.com>
2016-06-27 19:44:13 -0700

    moved view and models out from center into seperate places

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
-var ViewModel = function (){
+var Cat = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/phtos/big');
-  this.level = ko.observable('newBorn');
-  this.nickNames = ko.observableArray(['Mittens','Bubbles','Cuteness']);
+  this.nickNames = ko.observableArray([{name: 'Mittens'},{name :'Bubbles'},{name:'Cuteness'}]);
 
-  this.incrementCounter = function(){
-    if(this.clickCount()>5 && this.clickCount()<10){
-      this.clickCount(this.clickCount()+1);
-      this.level('infant');
+  this.level = ko.computed(function(){
+    var level;
+    var clicks = this.clickCount();
+    if(clicks>5 && clicks<10){
+      level = 'infant';
     }
-    else if(this.clickCount()>10){
-      this.clickCount(this.clickCount()+1);
-      this.level('teen');
+    else if(clicks>10){
+      level = 'teen';
     }
     else {
-      this.clickCount(this.clickCount()+1);
+      level = 'newborn';
     }
-  };
+      return level;
+  },this);
+
+
+
 }
+
+var ViewModel = function (){
+
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementCounter = function(){
+    this.currentCat().clickCount(this.currentCat().clickCount()+1);
+  };
+};
 
 ko.applyBindings(new ViewModel());
