64bd89c84bbeedac03841020a549365f8e51471b
andrew gibbs <andrewgibbs@outlook.com>
2016-06-29 08:31:05 -0700

    making cats list loop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+var intialCats= [
+  {
+    clickCount : 0,
+    name :'Tabby',
+    imgSrc :'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution : 'https://www.flickr.com/phtos/big',
+    nickNames :[{name: 'Mittens'},{name :'Bubbles'},{name:'Cuteness'}]
+  },
+  {
+    clickCount : 0,
+    name :'Meosers',
+    imgSrc :'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution : 'https://www.flickr.com/phtos/big',
+    nickNames :[{name: 'bob'},{name :'Precious'},{name:'Curtts'}]
+  }
+];
+
 var Cat = function(data){
   this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
@@ -27,13 +44,13 @@ var Cat = function(data){
 var ViewModel = function (){
   var self = this; // self is always defined as the view model
 
-  this.currentCat = ko.observable(new Cat({
-    clickCount : 0,
-    name :'Tabby',
-    imgSrc :'img/434164568_fea0ad4013_z.jpg',
-    imgAttribution : 'https://www.flickr.com/phtos/big',
-    nickNames :[{name: 'Mittens'},{name :'Bubbles'},{name:'Cuteness'}]
-  }));
+  this.catList = ko.observableArray([]);
+
+  intialCats.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function(){
     self.currentCat().clickCount(self.currentCat().clickCount()+1);
